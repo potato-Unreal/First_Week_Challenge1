e64be7d0ea609fc285f5acd6450a745a7529035c
potato-Unreal <alswo0103kk@naver.com>
2024-12-26 20:42:06 +0900

    homwork_cummit

--- Homework_Essential.cpp
+++ Homework_Essential.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int add(int a[]) {
+int add(int a[], int length) {
 	int result = 0;
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < length; i++) {
 		result += a[i];
 	}
 	return result;
 }
 
-double average(double a) {
-	double result = a / 5;
+double average(double a, int length) {
+	double result = a / length;
 	return result;
 }
 
 int main() {
-	int arr[5];
+	int length = 1;
+	int arr[1];
 
-	for (int i = 0; i < 5; i++) {
+	cout << "숫자를 몇개 입력하시겠습니까?" << endl;
+	cin >> length;
+
+	cout << "숫자를 " << length << "개 입력해주세요." << endl;
+	for (int i = 0; i < length; i++) {
 		cin >> arr[i];
 	}
 
-	int addResult = add(arr);
-	double avrResult = average(addResult);
+	int addResult = add(arr, length);
+	double avrResult = average(addResult, length);
 	cout << "합 : " << addResult << endl;
 	cout << "평균 : " << avrResult << endl;
 
